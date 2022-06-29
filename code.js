da12546fa1770bf719bc4f1f624ad54c3e202c31
Ona Narbutas <ona.narbutas@gmail.com>
2022-06-29 17:12:24 -0400

    Create index.html and code.js; add pseudocode to code.js

---
+++ code.js
@@ -0,0 +1,21 @@
+//Including just to confirm code.js is properly linked into index.html
+console.log("Hello world!");
+
+//Ask user to input rock/paper/scissors and store response as string in variable playerChoice
+//Convert playerChoice to all lowercase
+//If playerChoice does not strictly equal 'rock', 'paper', or 'scissors', reprompt user for choice.
+
+//Create variable computerChoice
+//Generate random number 3 times, storing each in variables random1, random2, and random3, respectively
+//If random1 is higher than random2 or random3, set computerChoice to string 'rock.'
+    //Else if random2 is highest, set computerChoice to string 'scissors'
+    //Else if random3 is highest, set computerChoice to string 'paper'
+
+
+//Log playerChoice and computerChoice to console.
+
+//Compare playerChoice to computerChoice as follows:
+//If playerChoice is equal to computerChoice, log 'It's a tie!' and go back to start
+//If playerChoice is rock and computerChoice is scissors, log 'You win!' and go back to start.
+//Else log 'you lose' and go back to start. **repeating go back to starts suggest switch statement may be best**
+//Repeat per RPS rules for cases of playerChoice 'scissors' and 'paper'.
