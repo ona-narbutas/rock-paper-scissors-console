2853783f591b1fd4c534b2c37b62005d2f419a4d
Ona Narbutas <ona.narbutas@gmail.com>
2022-06-29 18:22:53 -0400

    Added askPlayer() and rockPaperScissors() functions

--- code.js
+++ code.js
@@ -5,6 +5,22 @@ console.log("Hello world!");
 //Convert playerChoice to all lowercase
 //If playerChoice does not strictly equal 'rock', 'paper', or 'scissors', reprompt user for choice.
 
+function askPlayer() {
+    let output;
+
+    while (output !== 'rock' && output !== 'paper' && output !== 'scissors') {
+        output = prompt('Rock, paper, or scissors?');
+        output = output.toLowerCase();
+
+        //console.log(output);
+
+    }
+
+    return output;
+}
+
+playerSelection()
+
 //Create variable computerChoice
 //Generate random number 3 times, storing each in variables random1, random2, and random3, respectively
 //If random1 is higher than random2 or random3, set computerChoice to string 'rock.'
@@ -22,7 +38,7 @@ function computerPlay() {
         let random2 = Math.random();
         let random3 = Math.random();
 
-        console.log(random1 + ' ' + random2 + ' ' + random3);
+        //console.log(random1 + ' ' + random2 + ' ' + random3);
 
         if (random1 > random2 && random1 > random3) {
             output = 'rock';
@@ -35,7 +51,7 @@ function computerPlay() {
             keepGoing = false;
         } else keepGoing = true;
 
-        console.log(output + '\n' + keepGoing);
+        //console.log(output + '\n' + keepGoing);
 
     } while (keepGoing == true);
 
@@ -44,10 +60,36 @@ function computerPlay() {
 
 computerPlay();
 
-//Log playerChoice and computerChoice to console.
-
 //Compare playerChoice to computerChoice as follows:
-//If playerChoice is equal to computerChoice, log 'It's a tie!' and go back to start
-//If playerChoice is rock and computerChoice is scissors, log 'You win!' and go back to start.
-//Else log 'you lose' and go back to start. **repeating go back to starts suggest switch statement may be best**
+//If playerChoice is equal to computerChoice, return 'It's a tie!' and go back to start
+//If playerChoice is rock and computerChoice is scissors, return 'You win!' and go back to start.
+//Else return 'you lose' and go back to start. **repeating go back to starts suggest switch statement may be best**
 //Repeat per RPS rules for cases of playerChoice 'scissors' and 'paper'.
+
+function rockPaperScissors(playerSelection, computerChoice) {
+    let result;
+    
+    if (playerSelection == computerChoice) {
+        result = 'It\'s a tie!';
+        return result;
+    } else switch(playerSelection) {
+        case 'rock':
+            computerChoice == 'scissors' ? result = `You win! ${playerSelection} beats ${computerChoice}.`
+            : result = `You lose. ${computerChoice} beats ${playerSelection}.`;
+            return result;
+            break;
+
+        case 'paper':
+            computerChoice == 'rock' ? result = `You win! ${playerSelection} beats ${computerChoice}.`
+            : console.log(`You lose. ${computerChoice} beats ${playerSelection}.`);
+            return result;
+            break;
+
+        case 'scissors':
+            computerChoice == 'paper' ? result = `You win! ${playerSelection} beats ${computerChoice}.`
+            : result = `You lose. ${computerChoice} beats ${playerSelection}.`;
+            return result;
+            break;
+
+    }
+}
